da45bea66ea17cd3161157f11a6fc2331398d9dc
qwertyone <qwertyone@users.noreply.github.com>
2017-08-03 16:18:32 +0200

    fix

--- data-sets/Karlsruhe/twitter-token.R
+++ data-sets/Karlsruhe/twitter-token.R
@@ -16,4 +16,5 @@ twitter_token <- create_token(app=appname, consumer_key=key, consumer_secret=sec
 #save twitter token
 home_directory <- path.expand("~/")
 file_name <- file.path(home_directory, "twitter_token.rds")
+saveRDS(twitter_token, file = file_name)
 cat(paste0("TWITTER_PAT=", file_name), file = file.path(home_directory, ".Renviron"), append=TRUE)
